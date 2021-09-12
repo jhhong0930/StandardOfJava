bde2e81653758f0ab432f1a9470adac07979297c
Joe <jhhong0930@gmail.com>
2021-09-12 09:54:14 +0900

    클래스변수와 인스턴스변수

---
+++ src/main/java/com/java/chapter06/CardTest.java
@@ -0,0 +1,35 @@
+package com.java.chapter06;
+
+class Card {
+    String kind;
+    int number;
+    static int width = 100;
+    static int height = 250;
+}
+
+class CardTest {
+    public static void main(String[] args) {
+        System.out.println("Card.width = " + Card.width);
+        System.out.println("Card.height = " + Card.height);
+
+        Card c1 = new Card();
+        c1.kind = "Heart";
+        c1.number = 7;
+
+        Card c2 = new Card();
+        // 인스턴스변수의 값을 변경
+        c2.kind = "Spade";
+        c2.number = 4;
+
+        System.out.println("c1은 " + c1.kind + ", " + c1.number + "이며, 크기는 (" + c1.width + ", " + c1.height + ")");
+        System.out.println("c2는 " + c2.kind + ", " + c2.number + "이며, 크기는 (" + c2.width + ", " + c2.height + ")");
+
+        System.out.println("c1의 width와 height를 각각 50, 80으로 변경합니다.");
+        // 클래스변수의 값을 변경
+        c1.width = 50;
+        c1.height = 80;
+
+        System.out.println("c1은 " + c1.kind + ", " + c1.number + "이며, 크기는 (" + c1.width + ", " + c1.height + ")");
+        System.out.println("c2는 " + c2.kind + ", " + c2.number + "이며, 크기는 (" + c2.width + ", " + c2.height + ")");
+    }
+}
